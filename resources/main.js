2b2f91db23efad5e0fd39d0bff59747ea2a227a6
Jesse <31576321+agentjesse@users.noreply.github.com>
2018-01-27 17:39:25 -0500

    final

--- resources/main.js
+++ resources/main.js
@@ -38,9 +38,10 @@ d3.json('resources/countries.geo.json', function(error,data){ //paths from scrip
 	//next data call
 	d3.json('resources/data.json',function(error,data){
 		if(error)console.log(error);//super important. display error if found!!
-		// format data. in this case, toss items with null geometry values, and convert strings to num
-		data.features = data.features.filter( item => item.geometry );
-		data.features.forEach( d => { d.properties.mass = Number(d.properties.mass) });
+		// format data. in this case, toss items with null geometry/mass values, convert necessary strings to num, sort by mass descending to avoid inaccessible plots
+		data.features = data.features.filter( item => item.geometry && item.properties.mass );
+		data.features.forEach( d => { d.properties.mass = Number(d.properties.mass) } );
+		data.features.sort( (a,b)=> b.properties.mass - a.properties.mass );
 		//set data:
 		meteorData = data;
 		console.log('meteorites', data);
@@ -53,12 +54,23 @@ d3.json('resources/countries.geo.json', function(error,data){ //paths from scrip
 function fire(){
 	console.log('data loaded. intializing...');
 
+	// fine tuning function for circle plot radius from mass
+	function massToRadius(mass) {
+		if (mass < 10000) //tiny cutoff
+			return 1;
+		else if (mass < 100000) //small limit
+			return 2;
+		else if (mass > 1000000)//huge cutoff
+			return 16;
+		else
+			return massToRadiusMain(mass);//the rest can be scaled
+	}
 	//make scales
-	massToRadius = d3.scaleLinear()
-		.domain( d3.extent( meteorData.features, d => d.properties.mass ) )
-		.range([2,10]);
-	console.log(massToRadius.domain());
-
+	massToRadiusMain = d3.scaleLinear()
+		// .domain( d3.extent( meteorData.features, d => d.properties.mass ) )
+		.domain( [100000,1000000] )
+		.range([3,15]);
+	console.log(massToRadiusMain.domain());
 	
 	// make and set projection object
 	const mercatorProjection = d3.geoMercator()
@@ -78,7 +90,7 @@ function fire(){
 			.data(meteorData.features)//returns update selection
 		.enter().append('circle')//appending path for each data point in enter selection
 		// .filter( d => d.geometry === null ? false : true ) //already done above. data included null value geometry props, filter them out
-			.style('opacity', 0.6 )
+			.style('opacity', 0.5 )
 			.attr('fill', d => d3.interpolateRainbow( Math.random() ) )
 			.attr('r', d => massToRadius(d.properties.mass) )
 			.attr('cx', d => mercatorProjection( d.geometry.coordinates )[0] ) //pass in a [lat,lon] for current item to the projection object. you will get a [x,y]
@@ -86,55 +98,32 @@ function fire(){
 			//event handling
 			.on('mouseover', function(d,i){ //current datum and index
 				// console.log(d);
-				tooltip.html( `${ d.properties.name }<br>${ d.properties.mass }` )
+				tooltip.html(`
+						<span class='boldColor'>${ d.properties.name }</span> - ${ d.properties.year.slice(0,4) }<br>
+						mass: ${ d.properties.mass.toLocaleString( undefined, {style:'decimal'} ) }<br>
+						long: ${ d.geometry.coordinates[0].toFixed(2) } lat: ${ d.geometry.coordinates[1].toFixed(2) }<br>
+						recclass: ${ d.properties.recclass }
+					`)
 					//DON'T FORGET TO OFFSET THE POPUP OR IT WILL INTERFERE, causing multiple event firing
 					.style('left', d3.event.pageX + 15 + 'px')//d3.event must be used to access the usual event object
 					.style('top', d3.event.pageY - 20 + 'px');
-					tooltip.transition()//smooth transition, from d3: https://github.com/d3/d3-3.x-api-reference/blob/master/Selections.md#transition
-					.duration(700)//ms
-					.style('opacity', 1);
-				d3.select(this).style('opacity',1);
+				tooltip.transition()//smooth transition, from d3: https://github.com/d3/d3-3.x-api-reference/blob/master/Selections.md#transition
+					.duration(500)//ms
+					.style('opacity', 0.97);
+				d3.select(this).style('opacity',0.9);
 			})
 			.on('mouseout', function(d,i){
-				tooltip.style('opacity', 0)//reset opacity for next transition
-					.style('top', '-50px');//throw off screen to prevent interference.still appears if just nuking opacity
-				d3.select(this).style('opacity',0.5);
-			})
-			;
+				tooltip.style('opacity', 0)
+					.style('top', '-150px');//throw off screen to prevent interference.still appears if just nuking opacity without transition
+				d3.select(this).style('opacity',0.5);//reset opacity of plot
+			});
 	
 	//enable pan and zoom on a selection. works with html, svg, canvas
 	svg.call( //pick selection to fire event
 		d3.zoom() //make zoom behaviour , it is an object/function
 			.on("zoom", ()=> { //zoom event handles many types of zooms
-					group.attr("transform", d3.event.transform)
+					group.attr("transform", d3.event.transform) //magic
 				}
 			)
 	);
 }
-
-
-
-/*
-const node = nodesDiv.selectAll('.node')
-		.data(nodes)
-	.enter().append('img')
-		.attr('class', d => `node flag flag-${d.code}`)
-		.call(force.drag)
-		//event handling
-		.on('mouseover', function(d,i){ //current datum and index
-			// console.log(d);
-			tooltip.html( `${d.country}` )
-			//DON'T FORGET TO OFFSET THE POPUP OR IT WILL INTERFERE, causing multiple event firing
-			.style('left', d3.event.pageX + 15 + 'px')//d3.event must be used to access the usual event object
-			.style('top', d3.event.pageY - 20 + 'px');
-			tooltip.transition()//smooth transition, from d3: https://github.com/d3/d3-3.x-api-reference/blob/master/Selections.md#transition
-			.duration(700)//ms
-			.style('opacity', 1);
-			d3.select(this).style('opacity','0.4');
-		})
-		.on('mouseout', function(d,i){
-			tooltip.style('opacity', 0)//reset opacity for next transition
-				.style('top', '-50px');//throw off screen to prevent interference.still appears if just nuking opacity
-			d3.select(this).style('opacity','1');
-		});
-*/
